9284f1fd47b4ba5ed40dd7479ee7ed51c74dd7b6
Philip Bulley <philip@milkisevil.com>
2018-11-20 21:29:00 +0000

    Removes image references

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-import { Grid, Col, Row } from 'react-styled-flexboxgrid';
+import { Grid } from 'react-styled-flexboxgrid';
 import Header from './header';
 import './layout.css';
 import Nav from './nav';
