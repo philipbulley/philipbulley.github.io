23425dc607844abbdba5c0dce880165e9e40a2a6
Philip Bulley <philip@milkisevil.com>
2018-12-24 02:34:54 +0000

    Adds analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,8 +26,14 @@ module.exports = {
         // icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-131286876-1',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
