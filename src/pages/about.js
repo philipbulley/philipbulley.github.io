ee00789b1bf645f9e3fd8d878cfe6e8bce577ca1
Philip Bulley <philip@milkisevil.com>
2021-01-10 01:22:27 +0000

    Add C64 + minor text changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,40 +3,81 @@ import styled from 'styled-components';
 import query from '../shared/media';
 import Grid from '../shared/grid/grid';
 import posed from 'react-pose';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 
-const About = () => (
-  <Container initialPose="init" pose="show">
-    <Grid>
-      <SubHeading>A love for application development.</SubHeading>
-      <SubHeading>A passion for user interface.</SubHeading>
-      <AboutText>
-        <Paragraph>
-          With 19 years of industry experience, I've developed a keen eye for
-          visual design and an intuitive approach to application architecture. I
-          apply well-known software development patterns whilst being an
-          advocate of KISS, DRY and YAGNI principles.
-        </Paragraph>
+export const imageQuery = graphql`
+  query {
+    fileName: file(relativePath: { eq: "commodore-64-c.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 768) {
+          aspectRatio
+          base64
+          src
+          sizes
+          srcSet
+        }
+      }
+    }
+  }
+`;
 
-        <Paragraph>
-          My career spans the entire stack from dev ops to back-end to front-end
-          to UX design. Whilst I have a solid full-stack understanding, I’m most
-          at home working on the front-end at the intersection of clean
-          performant code and effective, engaging user interface design.
-        </Paragraph>
+const date = new Date();
+let years = date.getFullYear() - 1999;
+if (date.getMonth() < 7) {
+  years--;
+}
 
-        <Paragraph>
-          During my career, my advertising work has won awards at the highest
-          level; I’ve co-founded two tech start-ups;{' '}
-          <strong>
-            most recently, I’ve relished the challenges of working within
-            complex FinTech domains, delivering apps that shine in both form and
-            function.
-          </strong>
-        </Paragraph>
-      </AboutText>
-    </Grid>
-  </Container>
-);
+const About = () => {
+  const data = useStaticQuery(imageQuery);
+
+  return (
+    <Container initialPose="init" pose="show">
+      <Grid>
+        <SubHeading>A love for application development.</SubHeading>
+        <SubHeading>A passion for user interface.</SubHeading>
+        <AboutText>
+          <Paragraph>
+            With {years} years of industry experience, I've developed a keen eye
+            for visual design and an intuitive approach to application
+            architecture. I apply well-known software development patterns
+            whilst being an advocate of KISS, DRY (sometimes WET) and YAGNI
+            principles.
+          </Paragraph>
+
+          <Paragraph>
+            My career spans the entire stack from dev ops to back-end to
+            front-end to UX design. Whilst I have a solid full-stack
+            understanding, I’m most at home working on the front-end at the
+            intersection of clean performant code and effective, engaging user
+            interface design.
+          </Paragraph>
+
+          <Paragraph>
+            During my career, my advertising work has won awards at the highest
+            level; I’ve co-founded two tech start-ups;{' '}
+            <strong>
+              most recently, I’ve relished the challenges of working within
+              complex FinTech domains, delivering apps that shine in both form
+              and function.
+            </strong>
+          </Paragraph>
+
+          <Paragraph>
+            It's fair to say that things have come a long way since Santa
+            delivered this beauty on Christmas Day 1989!{' '}
+            <a href="https://en.wikipedia.org/wiki/Commodore_64">
+              <Image
+                fluid={data.fileName.childImageSharp.fluid}
+                alt="A Commodore 64 C computer"
+              />
+            </a>
+          </Paragraph>
+        </AboutText>
+      </Grid>
+    </Container>
+  );
+};
 
 export default About;
 
@@ -74,6 +115,10 @@ const AboutText = styled.div`
     color: ${({ theme }) => theme.color.one};
     font-weight: ${({ theme }) => theme.font.weightDemiBold};
   }
+`;
+
+const Image = styled(Img)`
+  margin: 50px 0;
 `;
 
 const Paragraph = posed.p({
