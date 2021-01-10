6b43d3c237154c4f6bcf0110fdde870c853c2278
Philip Bulley <philip@milkisevil.com>
2021-01-10 01:22:27 +0000

    Add C64 + minor text changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,13 +5,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `images`,
-    //     path: `${__dirname}/src/images`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
